c066083fa314c46ea3ba1db8fff5bc670a24f5be
kim-yu <kimberly.yu1@gmail.com>
2017-06-23 23:30:14 -0700

    Create functions to 1. get a team page given the division, gender, and team name and 2. get the team's roster in list format

---
+++ rosterParser.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from urllib import urlopen
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+def getTeamPage(division, gender, team, search):
+  results = []
+  url = urlopen('http://play.usaultimate.org/teams/events/team_rankings/?RankSet='+division+'-'+gender+'&F_'+search+'Name='+team)
+  content = url.read()
+  soup = BeautifulSoup(content, "html5lib")
+  table = soup.find("table", { "class": "global_table" })
+  tableBody = table.find('tbody')
+  try:
+    tr = tableBody.find('tr', { "class": "row" })
+    cols = tr.find_all('td')
+    teamPage = str('http://play.usaultimate.org'+cols[1].find('a').get('href'))
+  except:
+    print "no team page found"
+  return teamPage
+
+def getRoster(teamPage):
+  roster = []
+  url = urlopen(teamPage)
+  content = url.read()
+  soup = BeautifulSoup(content, "html5lib")
+  table = soup.find("table", { "class": "global_table" })
+  tableBody = table.find('tbody')
+  rows = tableBody.find_all('tr')
+  for r in rows[1:]:
+    info = r.find_all('td')
+    number = info[0].text
+    first, last = info[1].text.lower().split()
+    cleanedName = first[0].upper()+first[1:] + ' ' + last[0].upper()+last[1:]
+    roster.append((cleanedName, number))
+  return roster
+
+if __name__ == '__main__':
+  division = sys.argv[1]
+  gender = sys.argv[2]
+  team = sys.argv[3]
+  if division.lower() == 'college':
+    search = 'School'
+  else:
+    search = 'Team'
+  
+  teamPage = getTeamPage(division, gender, team, search)
+  roster = getRoster(teamPage)
+  print tabulate(roster, headers=['Name', 'Number'])
